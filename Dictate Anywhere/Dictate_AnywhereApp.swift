42fb016bdc6b805f2c716a58446deb872930b31e
hoomanaskari <69511785+hoomanaskari@users.noreply.github.com>
2026-01-14 14:04:31 -0500

    Initial Commit

---
+++ Dictate Anywhere/Dictate_AnywhereApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dictate_AnywhereApp.swift
+//  Dictate Anywhere
+//
+//  Created by Hooman on 1/14/26.
+//
+
+import SwiftUI
+
+@main
+struct Dictate_AnywhereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
