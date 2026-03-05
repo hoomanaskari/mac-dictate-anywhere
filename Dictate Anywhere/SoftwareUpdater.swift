728ef9938b15b99e5f7b609781f8df18ec398701
Hooman <69511785+hoomanaskari@users.noreply.github.com>
2026-03-05 10:35:59 -0500

    Keep Sparkle window on top

--- Dictate Anywhere/SoftwareUpdater.swift
+++ Dictate Anywhere/SoftwareUpdater.swift
@@ -11,17 +11,55 @@ import SwiftUI
 @Observable
 final class SoftwareUpdater {
     let updaterController: SPUStandardUpdaterController
+    private let userDriverDelegate = SparkleUserDriverDelegate()
 
     init() {
         updaterController = SPUStandardUpdaterController(
             startingUpdater: true,
             updaterDelegate: nil,
-            userDriverDelegate: nil
+            userDriverDelegate: userDriverDelegate
         )
     }
 
     func checkForUpdates() {
         updaterController.checkForUpdates(nil)
+    }
+}
+
+private final class SparkleUserDriverDelegate: NSObject, SPUStandardUserDriverDelegate {
+    func standardUserDriverWillShowModalAlert() {
+        bringSparkleWindowToFront()
+    }
+
+    func standardUserDriverDidShowModalAlert() {
+        bringSparkleWindowToFront()
+    }
+
+    func standardUserDriverWillHandleShowingUpdate(_ handleShowingUpdate: Bool, for update: SUAppcastItem, state: SPUUserUpdateState) {
+        bringSparkleWindowToFront()
+    }
+
+    func standardUserDriverWillFinishUpdateSession() {
+        restoreActivationPolicy()
+    }
+
+    private func bringSparkleWindowToFront() {
+        DispatchQueue.main.async {
+            NSApp.setActivationPolicy(.regular)
+            NSApp.activate(ignoringOtherApps: true)
+        }
+    }
+
+    private func restoreActivationPolicy() {
+        DispatchQueue.main.async {
+            switch Settings.shared.appAppearanceMode {
+            case .dockAndMenuBar:
+                NSApp.setActivationPolicy(.regular)
+            case .menuBarOnly:
+                let hasVisibleWindow = NSApp.windows.contains { $0.isVisible && $0.level == .normal }
+                NSApp.setActivationPolicy(hasVisibleWindow ? .regular : .accessory)
+            }
+        }
     }
 }
 
